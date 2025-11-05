d31ab125fbbacfcb98303b2baadc0f13a7265b6e
claudia <ximenatobias1@gmail.com>
2025-11-05 11:51:04 +0100

    Initial Commit

---
+++ DuolingoReplica/DuolingoReplicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuolingoReplicaApp.swift
+//  DuolingoReplica
+//
+//  Created by Claudia Tobias on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DuolingoReplicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DuolingoReplicaTests/DuolingoReplicaTests.swift
@@ -0,0 +1,17 @@
+//
+//  DuolingoReplicaTests.swift
+//  DuolingoReplicaTests
+//
+//  Created by Claudia Tobias on 05/11/25.
+//
+
+import Testing
+@testable import DuolingoReplica
+
+struct DuolingoReplicaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
